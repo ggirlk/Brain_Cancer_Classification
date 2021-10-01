728c7fa6656b8675232ff1304574a02464c17d4a
khouloud <ggirlk2@gmail.com>
2021-10-01 16:03:28 +0100

    delete

--- deployment/classification/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import *
-
-
-class brain_MRI_form(forms.ModelForm):
-
-    class Meta:
-        model = brain_MRI
-        fields = ['MRI_image']
--- deployment/deployment/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('classification/', include('classification.urls'))
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
